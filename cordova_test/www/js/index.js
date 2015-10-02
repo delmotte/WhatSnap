0d675f8c157ec403b7f536e64a687b521d5620f6
Fabien Pinel <pinel.fabien@gmail.com>
2015-10-02 19:10:03 +0200

    recreating cordova_test

--- cordova_test/www/js/index.js
+++ cordova_test/www/js/index.js
@@ -16,27 +16,6 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-
-function getContacts() {
-    navigator.contactsPhoneNumbers.list(function(contacts) {
-        console.log(contacts.length + ' contacts found');
-        for(var i = 0; i < contacts.length; i++) {
-            console.log(contacts[i].id + " - " + contacts[i].displayName);
-            for(var j = 0; j < contacts[i].phoneNumbers.length; j++) {
-                var phone = contacts[i].phoneNumbers[j];
-                console.log("===> " + phone.type + "  " + phone.number + " (" + phone.normalizedNumber+ ")");
-            }
-        }
-        window.plugins.sim.getSimInfo(function (response) {
-            console.log(response);
-        }, function (error) {
-            console.error(error);
-        });
-    }, function(error) {
-        console.error(error);
-    });
-}
-
 var app = {
     // Application Constructor
     initialize: function() {
@@ -55,7 +34,6 @@ var app = {
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
         app.receivedEvent('deviceready');
-        getContacts();
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
