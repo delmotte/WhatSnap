57229832c9f3e057d7d7178cd05447773f144884
Jean-Yves Delmotte <jean-yves.delmotte@greencom-networks.com>
2015-10-02 11:23:24 +0200

    add plugins

--- cordova_test/www/js/index.js
+++ cordova_test/www/js/index.js
@@ -16,6 +16,27 @@
  * specific language governing permissions and limitations
  * under the License.
  */
+
+function getContacts() {
+    navigator.contactsPhoneNumbers.list(function(contacts) {
+        console.log(contacts.length + ' contacts found');
+        for(var i = 0; i < contacts.length; i++) {
+            console.log(contacts[i].id + " - " + contacts[i].displayName);
+            for(var j = 0; j < contacts[i].phoneNumbers.length; j++) {
+                var phone = contacts[i].phoneNumbers[j];
+                console.log("===> " + phone.type + "  " + phone.number + " (" + phone.normalizedNumber+ ")");
+            }
+        }
+        window.plugins.sim.getSimInfo(function (response) {
+            console.log(response);
+        }, function (error) {
+            console.error(error);
+        });
+    }, function(error) {
+        console.error(error);
+    });
+}
+
 var app = {
     // Application Constructor
     initialize: function() {
