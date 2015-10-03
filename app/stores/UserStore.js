d275650195f3723ababe6dfa6c14758cbd75ebe2
Jean-Yves Delmotte <jean-yves.delmotte@greencom-networks.com>
2015-10-03 16:10:13 +0200

    code polymer comme si on avait cordova

---
+++ app/stores/UserStore.js
@@ -0,0 +1,51 @@
+var UserStore = (function () {
+
+    var _sim = {}, _contacts = {}, _conversations;
+
+    function init() {
+        /*window.plugins.sim.getSimInfo(function (sim) {
+            _sim = sim;
+            navigator.contactsPhoneNumbers.list(function(contacts) {
+                _contacts = contacts.map(function (contact) {
+                    return {
+                        phoneNumber: contact.phoneNumbers.length > 0 ? contact.phoneNumbers[0].normalizedNumber : null,
+                        name: contact.displayName
+                    };
+                });
+                Dispatcher.emit('INITIALISATION_DONE');
+            }, function() {
+                Dispatcher.emit('NO_CONTACT_LIST', {});
+            });
+        }, function () {
+            Dispatcher.emit('NO_SIM_DETECTED', {});
+        });*/
+        _sim = {phoneNumber: "+33786625527"}
+        _contacts = [];
+        Dispatcher.emit('INITIALISATION_DONE');
+    }
+
+    function matchConversationWithName(conversations) {
+        _conversations = conversations;
+        for (var i in _conversations) {
+            for (var j in _contacts) {
+                if ( _conversations[i].usersId.indexOf(_contacts[j].phoneNumber) > -1) {
+                    _conversations[i].name = _contacts[j].displayName;
+                }
+            }
+        }
+    }
+
+    return {
+        init: init,
+        matchConversationWithName: matchConversationWithName,
+        getSim: function () {
+            return _sim;
+        },
+        getContacts: function () {
+            return _contacts;
+        },
+        getConversations: function () {
+            return _conversations;
+        }
+    };
+})();
